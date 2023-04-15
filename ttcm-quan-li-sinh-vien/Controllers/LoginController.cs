d1dee0e3e3366b70f85c286c3f8345858bfcfe6e
Anh Tien Dep Trai <tiensd17052001@example.com>
2023-04-15 15:07:06 +0700

    create controller login

---
+++ ttcm-quan-li-sinh-vien/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ttcm_quan_li_sinh_vien.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
