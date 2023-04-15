2d89573da113d8c69c7c2b626a3158dba598806a
Anh Tien Dep Trai <tiensd17052001@example.com>
2023-04-15 13:35:41 +0700

    create project

---
+++ ttcm-quan-li-sinh-vien/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ttcm_quan_li_sinh_vien
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ttcm-quan-li-sinh-vien/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ttcm_quan_li_sinh_vien.MvcApplication" Language="C#" %>
